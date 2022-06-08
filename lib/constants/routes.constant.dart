78c78a4f9fc75e60bb0831e0cac257a0141c15a4
ittang_majoo <ahmad.faisal@majoo.id>
2022-06-08 22:02:18 +0700

    update routes constant

---
+++ lib/constants/routes.constant.dart
@@ -0,0 +1,26 @@
+class RoutesConstant {
+  static const String LOGIN = '/login';
+  static const String HOME = '/homepage';
+  // Order
+  static const String ORDER_LIST = '/order-list';
+  static const String ORDER = '/order';
+  static const String ORDER_DETAIL = '/order/:order_id';
+  static const String ORDER_INVOICE = '/order/:order_id/invoice';
+  static const String ORDER_ADD = '/order-add';
+  static const String ORDER_TRACKING = '/order/:order_id/tracking';
+  static const String SPLASH_SCREEN = '/splash-screen';
+  static const String MAIN_WIDGET = '/main-widget';
+  static const String STATISTIC = '/statistic';
+  // Expenditure
+  static const String EXPENDITURE_CURRENT_MONTH = '/expenditure/current-month';
+  static const String EXPENDITURE_FIXED_MONTHLY = '/expenditure/fixed-monthly';
+  static const String EXPENDITURE_INCOMING_AND_EXPENSES =
+      '/expenditure/incoming-and-expenses';
+  // Settings
+  static const String SETTING = '/settings-page';
+  static const String BLUETOOTH = '/bluetooth-page';
+  static const String SERVICES = '/services';
+  static const String CUSTOMER = '/customer';
+  static const String PAYMENT = '/payment';
+  static const String PROMOTION = '/promotion';
+}
